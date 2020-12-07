8586ca41f54f8c204340fbe38b1bd56bfb5b8edb
BogVin <spongebob69961@gmail.com>
2020-12-07 15:11:28 +0200

    Lab9

---
+++ lab9/app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from config import Config
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+login = LoginManager(app)
+login.login_view = 'login'
+login.login_message_category = 'info'
+login.session_protection = 'strong'
+
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = Admin(app, name='Admin Page', template_mode='bootstrap3')
+from .models import User, Post
+import app.forms as  fr
+admin.add_view(fr.UserAdminView(User, db.session))
+admin.add_view(fr.PostAdminView(Post, db.session))
+
+from app import views, models
