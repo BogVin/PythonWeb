5b4c43c6bd7876202ba772d00523f45db44c4c1d
BogVin <spongebob69961@gmail.com>
2021-01-11 11:40:27 +0200

    Exam work

--- kr/app/views.py
+++ kr/app/views.py
@@ -1,65 +1,93 @@
-from flask import render_template, flash , redirect, url_for, request
+from flask import render_template, flash , redirect, url_for, request, jsonify
 from app import app, db
 from app.forms import CreateProductForm, UpdateProductForm
 from .models import Product, Category
 import os
 
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/api/add', methods=['GET', 'POST'])
 def add_product():
-    cat = Category.query.all()
-    categories=[(i.id, i.category) for i in cat]
-    form = CreateProductForm()
-    form.category.choices = categories
-    if form.validate_on_submit():
-        product = Product(code=form.code.data, name=form.name.data, instock=form.instock.data,
-         number=form.number.data, cost=form.cost.data, 
-         description=form.description.data, category_id=form.category.data)
-        db.session.add(product)
-        db.session.commit()
-        flash("Product was created", category="info")
-        return redirect(url_for('products'))
-
-    return render_template('add.html', form=form)
-
-
-@app.route('/products', methods=['GET', 'POST'])
-def products():
-    q = request.args.get('q')
-    if q:
-        products = Product.query.filter(Product.name.contains(q) | Product.description.contains(q))
-    else:
-        products = Product.query.order_by(Product.code.desc())
+    req = request.json
+    if not req :
+        abort(400)
     
-    return render_template('products.html', products=products)
+    product = Product(code=req.get('code'), name=req.get('name'), instock=req.get('instock'), number=req.get('number'), cost=req.get('cost'), description=req.get('description'), category_id=req.get('category_id'))
+    
+    db.session.add(product)
+    db.session.commit()
+
+    return jsonify({'message' : 'The product has been created!'})
 
 
-@app.route('/product/<int:product_id>', methods=['GET', 'POST'])
+@app.route('/api/products', methods=['GET', 'POST'])
+def products():
+    products = Product.query.all()
+    output = []
+    for product in products:
+        product_data = {}
+        product_data['id'] = product.id
+        product_data['code'] = product.code
+        product_data['name'] = product.name
+        product_data['instock'] = product.instock
+        product_data['number'] = product.number
+        product_data['cost'] = product.cost
+        product_data['description'] = product.description
+        product_data['category_id'] = product.category_id
+        output.append(product_data)
+    
+    return jsonify({'products' : output})
+
+
+@app.route('/api/product/<int:product_id>', methods=['GET', 'POST'])
 def product(product_id):
-    cat = Category.query.all()
-    categories=[(i.id, i.category) for i in cat]
-    form = UpdateProductForm()
-    form.category.choices = categories
     product = Product.query.filter_by(id=product_id).first()
-    if form.validate_on_submit():
-        product.code = form.code.data
-        product.name = form.name.data
-        product.instock = form.instock.data
-        product.number = form.number.data
-        product.cost = form.cost.data
-        product.description = form.description.data
-        product.category_id = form.category.data
-        db.session.commit()
-        flash("Product was updated", category="info")
-        return redirect(url_for('product', product_id=product.id))
+    
+    if not product:
+        return jsonify({'message' : 'No found!'})
+    
+    product_data = {}
+    product_data['id'] = product.id
+    product_data['code'] = product.code
+    product_data['name'] = product.name
+    product_data['instok'] = product.instock
+    product_data['number'] = product.number
+    product_data['cost'] = product.cost
+    product_data['description'] = product.description
+    product_data['category_id'] = product.category_id
+    
+    return jsonify({'products' : product_data})
 
-    return render_template('product.html', product=product, form=form)
+
+@app.route('/api/update/<int:product_id>', methods=['GET', 'POST'])
+def update_product(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    
+    if not product:
+        return jsonify({'message' : 'No found!'})
+    
+    req = request.json
+    if not req :
+        abort(400)
+    
+    product.code=req.get('code')
+    product.name=req.get('name')
+    product.instock=req.get('instock')
+    product.number=req.get('number')
+    product.cost=req.get('cost')
+    product.description=req.get('description')
+    product.category_id=req.get('category_id')
+    
+    db.session.commit()
+
+    return jsonify({'message' : 'The product has been updated!'})
 
 
-@app.route('/products/<int:product_id>/delete', methods=['GET', 'POST'])
+@app.route('/api/products/<int:product_id>/delete', methods=['DELETE'])
 def delete_product(product_id):
     product = Product.query.get(product_id)
+    if not product:
+        return jsonify({'message' : 'No post found!'})
     db.session.delete(product)
     db.session.commit()
-    flash("Product was deleted", category="info")
-    return redirect(url_for('products'))
+
+    return jsonify({'message' : 'The product has been deleted!'})
